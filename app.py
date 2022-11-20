f638abd5ab681215bfed1ff30bb13a68be45f38b
PRANJAL TRIPATHI <pranjal.tripathi18@gmail.com>
2022-11-19 22:43:02 -0500

    Added Open API Swagger

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import datetime
 from flask import Flask, request, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Python-Flask-REST-API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
 
 @app.route('/',methods = ['POST', 'GET'])
 def hello_world():
@@ -14,5 +29,8 @@ def hello_world():
     else:
         return jsonify(hello='World', method='GET')
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
